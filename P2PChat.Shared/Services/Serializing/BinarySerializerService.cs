354d6445aeab2835510ad4d0ee5a2d06aafb9d1f
Optifun <frdima19@gmail.com>
2021-06-25 18:55:32 +0700

    Add PacketSerializerService

---
+++ P2PChat.Shared/Services/Serializing/BinarySerializerService.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace P2PChat.Services.Serializing
+{
+	public class BinarySerializerService : IPacketSerializerService
+	{
+		private BinaryFormatter _fmt;
+
+		public BinarySerializerService()
+		{
+			_fmt = new BinaryFormatter();
+		}
+
+		public byte[] Serialize<T>(T obj) where T: class
+		{
+			var stream = new MemoryStream();
+			_fmt.Serialize(stream, obj);
+			return stream.ToArray();
+		}
+
+		public T Deserialize<T>(byte[] buffer) where T: class
+		{
+			var stream = new MemoryStream(buffer);
+			return _fmt.Deserialize(stream) as T;
+		}
+	}
+}
---
+++ P2PChat.Shared/Services/Serializing/IPacketSerializerService.cs
@@ -0,0 +1,8 @@
+﻿namespace P2PChat.Services.Serializing
+{
+	public interface IPacketSerializerService
+	{
+		byte[] Serialize<T>(T obj) where T : class;
+		T Deserialize<T>(byte[] buffer) where T : class;
+	}
+}
