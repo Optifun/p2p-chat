2b5894b2170deb241a70dd2d1f0171a35bff3e07
Optifun <frdima19@gmail.com>
2021-06-27 11:52:36 +0700

    Add base MessagePack Serializer service

---
+++ P2PChat.Shared/Services/Serializing/MsgPackSerializerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using MessagePack;
+
+namespace P2PChat.Services.Serializing
+{
+	public class MsgPackSerializerService : IPacketSerializerService
+	{
+		public byte[] Serialize<T>(T obj) where T : class
+		{
+			var bin = MessagePackSerializer.Serialize(typeof(T), obj,
+				MessagePack.Resolvers.ContractlessStandardResolver.Options);
+			return bin;
+		}
+
+		public T Deserialize<T>(byte[] buffer) where T : class
+		{
+			ReadOnlyMemory<byte> bin = new ReadOnlyMemory<byte>(buffer);
+			var obj = MessagePackSerializer.Deserialize(typeof(T), bin,
+				MessagePack.Resolvers.ContractlessStandardResolver.Options);
+			return obj as T;
+		}
+	}
+}
