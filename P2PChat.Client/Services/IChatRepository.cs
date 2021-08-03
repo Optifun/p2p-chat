cf4e96e0bcde309cb398ee662a7a97aa578ef421
Optifun <frdima19@gmail.com>
2021-08-03 17:14:57 +0700

    Add Chat and Message Repository interfaces to segregate responsibilities

---
+++ P2PChat.Client/Services/IChatRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P2PChat.Client.Services
+{
+    public interface IChatRepository
+    {
+        event Action<List<PublicUser>> UsersUpdated;
+        public List<PublicUser> Users { get; }
+        void ObserveUsers(int refreshTime);
+        void StopObservation();
+    }
+}
---
+++ P2PChat.Client/Services/IMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using P2PChat.Packets;
+
+namespace P2PChat.Client.Services
+{
+    public interface IMessageRepository
+    {
+        public event Action<Message> MessageReceived;
+        Dictionary<PublicUser, List<Message>> Chats { get; }
+    }
+}
