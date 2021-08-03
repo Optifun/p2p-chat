c96c7c387188b41426220195e92c8dd64b703caa
Optifun <frdima19@gmail.com>
2021-08-03 17:44:41 +0700

    Add ChatRepository to store users

--- P2PChat.Client/Routes/UserObserver.cs
+++ P2PChat.Client/Routes/UserObserver.cs
@@ -7,18 +7,19 @@ using P2PChat.Reciever;
 using P2PChat.Packets;
 using System.Diagnostics;
 using System.Net;
+using System.Threading;
 
 namespace P2PChat.Client.Routes
 {
-	class UserObserver : AbstractRoute
-	{
-		public event Action<List<PublicUser>> UsersReceived;
+    public class UserObserver : AbstractRoute
+    {
+        public event Action<List<PublicUser>> UsersReceived;
 
-		public override Action Handle(IPEndPoint sender, IPacket obj)
-		{
-			if ( obj is OnlineUsers users && users.Users != null && users.Action!= FetchAction.Null)
-				return () => UsersReceived?.Invoke(users.Users);
-			return base.Handle(sender, obj);
-		}
-	}
+        public override Action Handle(IPEndPoint sender, IPacket obj)
+        {
+            if (obj is OnlineUsers users && users.Users != null && users.Action != FetchAction.Null)
+                return () => UsersReceived?.Invoke(users.Users);
+            return base.Handle(sender, obj);
+        }
+    }
 }
---
+++ P2PChat.Client/Services/ChatRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using P2PChat.Client.Routes;
+using P2PChat.Packets;
+using P2PChat.Reciever;
+
+namespace P2PChat.Client.Services
+{
+    public class ChatRepository : IChatRepository, IDisposable
+    {
+        public event Action<List<PublicUser>> UsersUpdated;
+        public List<PublicUser> Users { get; protected set; }
+
+        private readonly UDPObserver _observer;
+        private readonly int _openedPort;
+        private readonly IPEndPoint _stunIp;
+        private readonly UserObserver _userObserver;
+        private CancellationTokenSource _tokenSource;
+        private Task _observationTask;
+
+        public ChatRepository(UDPObserver observer, UserObserver userObserver, int openedPort, IPEndPoint stunIp)
+        {
+            _userObserver = userObserver;
+            _stunIp = stunIp;
+            _openedPort = openedPort;
+            _observer = observer;
+        }
+
+        public void ObserveUsers(int refreshTime)
+        {
+            void RefreshLoop(object tokenObj)
+            {
+                var token = (CancellationToken) tokenObj;
+                while (!token.IsCancellationRequested)
+                {
+                    _fetchUsers();
+                    Thread.Sleep(refreshTime);
+                }
+            }
+
+            _userObserver.UsersReceived += AddUser;
+            _tokenSource = new CancellationTokenSource();
+            _observationTask = Task.Factory.StartNew(RefreshLoop, _tokenSource.Token, TaskCreationOptions.LongRunning);
+        }
+
+        private void AddUser(List<PublicUser> users)
+        {
+            Users.AddRange(users);
+            Users = Users.Distinct().ToList();
+        }
+
+        public void StopObservation()
+        {
+            if (_observationTask != null)
+                _tokenSource?.Cancel();
+        }
+
+        private void _fetchUsers()
+        {
+            var packet = new OnlineUsers(new List<PublicUser>(), FetchAction.Fetch, _openedPort);
+            _observer.Send(packet, _stunIp);
+        }
+
+        public void Dispose() =>
+            StopObservation();
+    }
+}
